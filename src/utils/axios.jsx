c1be04f9947279e7ac1924a39c9b777e42a4d9eb
gajendraverma <gajendraverma353@gmail.com>
2024-06-24 14:06:04 +0530

    UPDATE

--- src/utils/axios.jsx
+++ src/utils/axios.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 
+
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3/",
+    baseURL: "https://api.themoviedb.org/3",
     headers: {
         accept: "application/json",
-        Authorization: import.meta.env.VITE_REACT_APP_TOKEN,
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDViOWQ3NmNmZTcwMjNkYjVkNzFiMWZiZTNjOTMxNSIsIm5iZiI6MTcxOTIxNjM2MS4xMDM3MzMsInN1YiI6IjY2Nzg5NzQ1MzdjZGFmMTQ4MDNiMjliMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fIiqPy-XpRQJ5MShjXIyxUMlbh1PweSc6Y6yBCYPnUQ'
     },
 });
 
