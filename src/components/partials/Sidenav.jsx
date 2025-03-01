11e5e085e2c87eab9dba12951cc6461ea4ac8f1c
gajendraverma <gajendraverma353@gmail.com>
2025-03-01 15:51:50 +0530

    #

--- src/components/partials/Sidenav.jsx
+++ src/components/partials/Sidenav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Sidenav = () => {
     return (
-        <div className="w-[20%] h-full  border-r-2 border-zinc-400 p-10">
+        <div className="w-[20%] h-full   border-r-2 border-zinc-400 bg p-10">
             <h1 className="text-2xl text-white font-bold">
                 <i className="text-[#6556CD] ri-tv-fill mr-2"></i>
                 <span className="text-2xl">Movie Mania</span>
@@ -48,6 +48,7 @@ const Sidenav = () => {
                     People
                 </Link>
             </nav>
+            {/* <div className="bg-red-600">
             <hr className="border-none h-[1px] bg-zinc-400" />
             <nav className="flex flex-col text-zinc-400 text-xl gap-3">
                 <h1 className="text-white font-semibold text-xl mt-10 mb-5">
@@ -61,6 +62,7 @@ const Sidenav = () => {
                     Contact Us
                 </Link>
             </nav>
+            // </div> */}
         </div>
     );
 };
